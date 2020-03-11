b99dee98c52ffb6a1d25840a170a71f85138aa32
AkiBeulah <54029632+AkiBeulah@users.noreply.github.com>
2020-03-11 14:34:06 +0100

    Forgot password finishing

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,17 @@
+class UserMailer < ApplicationMailer
+    
+    def welcome_email(user)
+        @user = user
+        mail(to: @user.email, subject: 'Welcome to XpressChow')
+    end
+
+    def login_warning(user)
+        @user = user
+        mail(to: @user.email, subject: 'Your account is being accessed')
+    end
+
+    def forgot_password(user) {
+        @user = user
+        mail(to: @user.email, subject: 'Resetting your XpressChow Password')
+    }
+end
