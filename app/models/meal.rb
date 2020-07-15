6f8dfdb81f7bbf45929bda6c33503802c067e746
Akindele Beulah <akibeulah@gmail.com>
2020-07-15 04:13:26 +0100

    Further validation correction.

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -5,9 +5,9 @@ class Meal < ApplicationRecord
     # has_one :sample
     belongs_to :vendor, required: true, dependent: :destroy
 
-    validates :sample, presence: true
-    validates :name, presence: true
-    validates :price, presence: true
+    #validates :sample, presence: true
+    #validates :name, presence: true
+    #validates :price, presence: true
 
     pg_search_scope :search_meals,
     against: { name: :A, desc: :C, tag: :B },
